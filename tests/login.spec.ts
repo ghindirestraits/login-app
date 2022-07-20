9add41d05f4218d4be487b599e2b6191f55cd66a
Gabriel Heberle <gheberledude@gmail.com>
2022-07-20 20:03:18 -0300

    Improve page object instantiation

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,22 +1,32 @@
 import { test } from '@playwright/test';
 import { LoginPage } from '../pages/login-page';
 
+let loginPage: LoginPage;
+
+test.beforeEach(async ({ page }) => {
+    loginPage = new LoginPage(page);
+});
+
 test('deve logar com sucesso', async ({ page }) => {
-
-    const loginPage: LoginPage = new LoginPage(page);
-
     await loginPage.go();
     await loginPage.signIn('qa', 'cademy');
     await loginPage.userLoggedIn();
-
 });
 
 test('senha incorreta', async ({ page }) => {
-
-    const loginPage: LoginPage = new LoginPage(page);
-
     await loginPage.go();
     await loginPage.signIn('qa', 'abc123');
     await loginPage.toastMessage('Oops! Credenciais inválidas :(');
+});
 
+test('nome obrigatório', async ({ page }) => {
+    await loginPage.go();
+    await loginPage.signIn('', 'cademy');
+    await loginPage.toastMessage('Informe o seu nome de usuário!');
+});
+
+test('senha obrigatória', async ({ page }) => {
+    await loginPage.go();
+    await loginPage.signIn('qa', '');
+    await loginPage.toastMessage('Informe a sua senha secreta!');
 });
